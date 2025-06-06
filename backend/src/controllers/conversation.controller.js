968af8a30af9cc0cdad6914b42965378c5500915
Miguel Cabrera Victoria <miguelangelcabreravictoria@gmail.com>
2025-06-05 19:01:15 -0600

    feat: agrega logger para actividades y gpt

--- backend/src/controllers/conversation.controller.js
+++ backend/src/controllers/conversation.controller.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import logger from '../utils/logger'
+import { response } from "express";
 
 export const conversation = async (req, res) => {
     try {
@@ -7,12 +9,15 @@ export const conversation = async (req, res) => {
             return res.status(400).json({ error: "Prompt is required" });
         }
 
+        const userID = req.user?.userId;
+        logger.activityLogger.info(`User ${userId} sent prompt: ${prompt}`);
         // Llama al modelo externo (reemplaza "url" por la URL real)
         const modelResponse = await axios.post("url", { prompt });
+        logger.gptLogger.info(`Response: ${prompt}`);
 
         res.json({ response: modelResponse.data });
     } catch (error) {
-        console.error(error);
+        logger.gptLogger(`Error to connect to GPT`)
         res.status(500).json({ error: "Fail" });
     }
 }
